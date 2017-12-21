54337fd87b5e43229e4a98132f089ab233c992e3
Guy Elsmore-Paddock <guy@rosieapp.com>
2017-12-21 16:17:31 -0500

    [AUTH-70] Sanitize Errors for Message Format
    The ICF logger runs everything through `MessageFormat.format()` which causes problems if we put curly braces in the output (for example, when printing out a server's JSON response payload). We now have a handy utility method to escape this.

---
+++ rosie-json-api-framework/src/main/java/com/rosieapp/services/common/util/Strings.java
@@ -0,0 +1,23 @@
+package com.rosieapp.services.common.util;
+
+/**
+ * A class for common utility methods needed when dealing with Strings.
+ */
+public class Strings {
+  /**
+   * Converts a string into a format that is suitable for being safely run through
+   * {@link java.text.MessageFormat#format(Object)}.
+   * <p>
+   * This is typically required if curly braces (e.g. <code>{</code> or <code>}</code>) will be
+   * included within the string, as these characters usually indicate the start of argument
+   * references for {@code MessageFormat}.
+   *
+   * @param   string
+   *          The string to escape.
+   *
+   * @return  The escaped string.
+   */
+  public static String escapeForMessageFormat(final String string) {
+    return string.replaceAll("([{}])", "'$1'");
+  }
+}
