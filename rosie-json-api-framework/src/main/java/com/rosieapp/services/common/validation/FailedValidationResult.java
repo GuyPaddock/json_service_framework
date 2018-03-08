e9e2e1fc7b79e7446bc1b4147a9148d398746be4
Guy Elsmore-Paddock <guy@rosieapp.com>
2018-03-07 21:00:42 -0500

    [RJAJ-12] Re-factor Enrollment Fields
    - Adds a common framework for performing frequently-needed validations (similar to `CommonValidations` from ECOM and LOYAL).
    - Adds a validation that the machine name in enrollment fields is _actually_ a machine name.
    - Moves the concern of normalizing an incoming enrollment field map into the `EnrollmentField` class and out of the `EnrollmentFieldSet` class, since all the data we need to work with is actually in the `EnrollmentField` class.
    - Fixes the constructor visibility for EnrollmentField to enforce that we need to go through the builder.
    - Cleans-up the tests for enrollment fields + updates them for the revised behavior.

---
+++ rosie-json-api-framework/src/main/java/com/rosieapp/services/common/validation/FailedValidationResult.java
@@ -0,0 +1,31 @@
+package com.rosieapp.services.common.validation;
+
+/**
+ * The type of validation result returned when a value fails validation.
+ *
+ * <p>A message describing the issue with the value can be returned by calling {@link #toString()}.
+ */
+public class FailedValidationResult
+implements ValidationResult {
+  private final String errorMessage;
+
+  /**
+   * Constructor for {@code FailedValidationResult}.
+   *
+   * @param errorMessage
+   *        The error message that indicates why the value failed validation.
+   */
+  public FailedValidationResult(final String errorMessage) {
+    this.errorMessage = errorMessage;
+  }
+
+  @Override
+  public boolean isValid() {
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return this.errorMessage;
+  }
+}
---
+++ rosie-json-api-framework/src/main/java/com/rosieapp/services/common/validation/SuccessfulValidationResult.java
@@ -0,0 +1,17 @@
+package com.rosieapp.services.common.validation;
+
+/**
+ * The type of validation result returned when a value passes validation.
+ */
+public class SuccessfulValidationResult
+implements ValidationResult {
+  @Override
+  public boolean isValid() {
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "";
+  }
+}
