55de5e38e4456110e57eb3830c3cfe1e73de0079
Guy Elsmore-Paddock <guy@rosieapp.com>
2018-04-12 13:38:22 -0400

    [RJAJ-6] Add Test for `AbstractFieldDependencyHandler`

---
+++ rosie-json-api-framework/src/test/java/com/rosieapp/services/common/model/fieldhandling/AbstractFieldDependencyHandlerTest.java
@@ -0,0 +1,90 @@
+package com.rosieapp.services.common.model.fieldhandling;
+
+import static com.greghaskins.spectrum.dsl.specification.Specification.beforeEach;
+import static com.greghaskins.spectrum.dsl.specification.Specification.context;
+import static com.greghaskins.spectrum.dsl.specification.Specification.describe;
+import static com.greghaskins.spectrum.dsl.specification.Specification.it;
+import static com.greghaskins.spectrum.dsl.specification.Specification.let;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import com.greghaskins.spectrum.Spectrum;
+import com.greghaskins.spectrum.Variable;
+import java.util.function.Supplier;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+@RunWith(Spectrum.class)
+@SuppressWarnings({
+  "Convert2MethodRef",
+  "CodeBlock2Expr",
+  "ClassInitializerMayBeStatic",
+  "Duplicates"
+})
+public class AbstractFieldDependencyHandlerTest {
+  {
+    describe("#handleOptionalField", () -> {
+      final Variable<String> fieldValue   = new Variable<>();
+      final Variable<String> defaultValue = new Variable<>();
+
+      final Supplier<FieldDependencyHandler> subject =
+        let(() -> mock(AbstractFieldDependencyHandler.class, Mockito.CALLS_REAL_METHODS));
+
+      final Supplier<String> result = let(() -> {
+        return subject.get().handleOptionalField(fieldValue.get(), "testField", defaultValue.get());
+      });
+
+      context("when the field value is `null`", () -> {
+        beforeEach(() -> {
+          fieldValue.set(null);
+        });
+
+        context("when the default field value is `null`", () -> {
+          beforeEach(() -> {
+            defaultValue.set(null);
+          });
+
+          it("returns `null`", () -> {
+            assertThat(result.get()).isNull();
+          });
+        });
+
+        context("when the default field value is not `null`", () -> {
+          beforeEach(() -> {
+            defaultValue.set("default value");
+          });
+
+          it("returns the default value", () -> {
+            assertThat(result.get()).isEqualTo("default value");
+          });
+        });
+      });
+
+      context("when the field value is not `null`", () -> {
+        beforeEach(() -> {
+          fieldValue.set("field value");
+        });
+
+        context("when the default field value is `null`", () -> {
+          beforeEach(() -> {
+            defaultValue.set(null);
+          });
+
+          it("returns the field value", () -> {
+            assertThat(result.get()).isEqualTo("field value");
+          });
+        });
+
+        context("when the default field value is not `null`", () -> {
+          beforeEach(() -> {
+            defaultValue.set("default value");
+          });
+
+          it("returns the field value", () -> {
+            assertThat(result.get()).isEqualTo("field value");
+          });
+        });
+      });
+    });
+  }
+}
